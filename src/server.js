0e26a9dc517dade3f7df014c46eed9b67cf88af9
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2021-01-27 13:17:55 -0300

    fix: errors deploy

--- src/server.js
+++ src/server.js
@@ -6,7 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("./database/connection");
 const routes_1 = __importDefault(require("./routes"));
+const PORT = process.env.PORT || 3333;
 const app = express_1.default();
 app.use(express_1.default.json());
 app.use(routes_1.default);
-app.listen(3333);
+app.listen(PORT, () => {
+    console.log(`API rodando na porta ${PORT}`);
+});
