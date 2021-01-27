135fae83940badee35fd288abe4f722d06308a95
Kauê Cavalcante <kaue_cavalcante.cnt@outlook.com>
2021-01-27 12:13:51 -0300

    mod: changed port listened

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,13 @@ import './database/connection';
 
 import routes from './routes'
 
+const PORT = process.env.PORT || 3333;
+
 const app = express();
 
 app.use(express.json());
 app.use(routes)
 
-app.listen(3333);
+app.listen(PORT, () => {
+  console.log(`API rodando na porta ${PORT}`);
+});
